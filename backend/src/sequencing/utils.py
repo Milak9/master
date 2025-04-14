019a566c913aad38eb5a7f3b9892821d5a588674
Milak9 <milos.milakovic98@gmail.com>
2025-04-15 00:32:49 +0200

    Update response from BE with necessary data for FE

--- backend/src/sequencing/utils.py
+++ backend/src/sequencing/utils.py
@@ -11,13 +11,25 @@ def linear_spectrum(peptide):
         prefix_mass[i + 1] = prefix_mass[i] + AMINO_ACID_MASSES[amino_acid]
 
     spectrum = [0]
+    spectrum_with_subpeptides = [
+        {
+            "mass": 0,
+            "subpeptide": ""
+        }
+    ]
 
     for i in range(n):
         for j in range(i + 1, n + 1):
-            spectrum.append(prefix_mass[j] - prefix_mass[i])
+            fragment_mass = prefix_mass[j] - prefix_mass[i]
+            spectrum.append(fragment_mass)
+            spectrum_with_subpeptides.append({
+                "mass": fragment_mass,
+                "subpeptide": peptide[i:j]
+            })
 
     spectrum.sort()
-    return spectrum
+    spectrum_with_subpeptides_sorted = sorted(spectrum_with_subpeptides, key=lambda x: x["mass"])
+    return spectrum, spectrum_with_subpeptides_sorted
 
 
 def cyclic_spectrum(peptide):
@@ -30,18 +42,33 @@ def cyclic_spectrum(peptide):
         prefix_mass[i + 1] = prefix_mass[i] + AMINO_ACID_MASSES[amino_acid]
 
     spectrum = [0]
+    spectrum_with_subpeptides = [
+        {
+            "mass": 0,
+            "subpeptide": ""
+        }
+    ]
     peptide_mass = prefix_mass[-1]
 
     for i in range(n):
         for j in range(i + 1, n + 1):
             fragment_mass = prefix_mass[j] - prefix_mass[i]
             spectrum.append(fragment_mass)
+            spectrum_with_subpeptides.append({
+                "mass": fragment_mass,
+                "subpeptide": peptide[i:j]
+            })
 
             if i > 0 and j < n:
                 spectrum.append(peptide_mass - fragment_mass)
+                spectrum_with_subpeptides.append({
+                    "mass": peptide_mass - fragment_mass,
+                    "subpeptide": peptide[j:n] + peptide[0:i]
+                })
 
     spectrum.sort()
-    return spectrum
+    spectrum_with_subpeptides_sorted = sorted(spectrum_with_subpeptides, key=lambda x: x["mass"])
+    return spectrum, spectrum_with_subpeptides_sorted
 
 
 def extend(peptides, amino_acid_candidates=AMINO_ACID_MASSES.keys()):
@@ -51,6 +78,26 @@ def extend(peptides, amino_acid_candidates=AMINO_ACID_MASSES.keys()):
         for amino_acid in amino_acid_candidates:
             if amino_acid != "":
                 extended_peptides.append(peptide + amino_acid)
+
+    return extended_peptides
+
+
+def extend_for_tree(peptides, tree, amino_acid_candidates=AMINO_ACID_MASSES.keys()):
+    extended_peptides = []
+
+    for peptide in peptides:
+        for amino_acid in amino_acid_candidates:
+            if amino_acid != "":
+                new_candidate = peptide + amino_acid
+                extended_peptides.append(new_candidate)
+                tree[new_candidate] = {
+                    "children": [],
+                    "end": False
+                }
+                if peptide == "":
+                    tree["Root"]["children"].append(new_candidate)
+                else:
+                    tree[peptide]["children"].append(new_candidate)
 
     return extended_peptides
 
@@ -65,7 +112,7 @@ def calculate_peptide_mass(peptide):
 
 
 def is_consistent_with_spectrum(peptide, target_spectrum):
-    peptide_spectrum = linear_spectrum(peptide)
+    peptide_spectrum, _ = linear_spectrum(peptide)
 
     i = 0
     j = 0
@@ -109,32 +156,41 @@ def score(peptide_spectrum, target_spectrum):
 
 
 def linear_score(peptide, target_spectrum):
-    peptide_linear_spectrum = linear_spectrum(peptide)
-    return score(peptide_linear_spectrum, target_spectrum)
+    peptide_linear_spectrum, spectrum_with_subpeptides_sorted = linear_spectrum(peptide)
+    return score(peptide_linear_spectrum, target_spectrum), spectrum_with_subpeptides_sorted
 
 
 def cyclic_score(peptide, target_spectrum):
-    peptide_cyclic_spectrum = cyclic_spectrum(peptide)
-    return score(peptide_cyclic_spectrum, target_spectrum)
+    peptide_cyclic_spectrum, spectrum_with_subpeptides = cyclic_spectrum(peptide)
+    return score(peptide_cyclic_spectrum, target_spectrum), spectrum_with_subpeptides
 
 
 def trim(peptides, target_spectrum, max_number_of_candidates):
-    if len(peptides) <= max_number_of_candidates:
-        return peptides
-
     leaderboard = []
 
     for peptide in peptides:
-        peptide_score = linear_score(peptide, target_spectrum)
-        leaderboard.append((peptide_score, peptide))
-    leaderboard.sort(reverse=True)
+        peptide_mass = calculate_peptide_mass(peptide)
+        peptide_score, spectrum_with_subpeptides_sorted = linear_score(peptide, target_spectrum)
+        current_candidate = {
+            "peptide": peptide,
+            "mass": peptide_mass,
+            "number_of_matches": peptide_score,
+            "spectrum": spectrum_with_subpeptides_sorted,
+            "qualified": False
+        }
+        leaderboard.append(current_candidate)
+
+    leaderboard = sorted(leaderboard, reverse=True, key=lambda x: x["number_of_matches"])
+    if len(peptides) <= max_number_of_candidates:
+        leaderboard = [{**item, "qualified": True} for item in leaderboard]
+        return peptides, leaderboard
 
     for i in range(max_number_of_candidates, len(leaderboard)):
-        if leaderboard[i][0] < leaderboard[max_number_of_candidates - 1][0]:
+        if leaderboard[i]["number_of_matches"] < leaderboard[max_number_of_candidates - 1]["number_of_matches"]:
             break
 
     trimmed_leaderboard = leaderboard[:i]
-    return [el[1] for el in trimmed_leaderboard]
+    return [el["peptide"] for el in trimmed_leaderboard], leaderboard
 
 
 def prepare_amino_acids_that_are_candidates(top_masses):
@@ -149,26 +205,48 @@ def prepare_amino_acids_that_are_candidates(top_masses):
 def leaderboard_sequencing(target_spectrum, amino_acid_candidates=AMINO_ACID_MASSES.keys()):
     peptides = ['']
 
-    leader_peptide = ''
+    leaderboard = []
+    current_round = 0
+    leader_peptide = []
     leader_peptide_score = 0
 
     target_peptide_mass = target_spectrum[-1]
 
     while len(peptides) > 0:
+        leaderboard.append([])
         extended_peptides = extend(peptides, amino_acid_candidates)
 
         consistent_peptides = []
-
         for peptide in extended_peptides:
             peptide_mass = calculate_peptide_mass(peptide)
+
             if peptide_mass == target_peptide_mass:
-                peptide_score = cyclic_score(peptide, target_spectrum)
+                peptide_score, spectrum_with_subpeptides_sorted = cyclic_score(peptide, target_spectrum)
+
+                current_candidate = {
+                    "peptide": peptide,
+                    "mass": peptide_mass,
+                    "number_of_matches": peptide_score,
+                    "spectrum": spectrum_with_subpeptides_sorted,
+                    "qualified": False
+                }
+
                 if peptide_score > leader_peptide_score:
-                    leader_peptide = peptide
+                    leader_peptide = [current_candidate]
                     leader_peptide_score = peptide_score
+                elif peptide_score == leader_peptide_score:
+                    leader_peptide.append(current_candidate)
+
             elif peptide_mass < target_peptide_mass:
                 consistent_peptides.append(peptide)
 
-        peptides = trim(consistent_peptides, target_spectrum, MAX_NUMBER_OF_CANDIDATES)
+        peptides, current_round_peptides = trim(consistent_peptides, target_spectrum, MAX_NUMBER_OF_CANDIDATES)
+        leaderboard[current_round] = current_round_peptides
+        current_round += 1
 
-    return leader_peptide, None
+    response = {
+        "leaderboard": leaderboard,
+        "solution": leader_peptide,
+        "N": MAX_NUMBER_OF_CANDIDATES
+    }
+    return response
--- backend/src/sequencing/views.py
+++ backend/src/sequencing/views.py
@@ -4,8 +4,8 @@ from django.utils.decorators import method_decorator
 from django.views.generic.base import View
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from .utils import (calculate_peptide_mass, cyclic_spectrum, is_consistent_with_spectrum, extend, leaderboard_sequencing,
-                   prepare_amino_acids_that_are_candidates)
+from .utils import (calculate_peptide_mass, cyclic_spectrum, is_consistent_with_spectrum, extend_for_tree,
+                    leaderboard_sequencing, prepare_amino_acids_that_are_candidates)
 
 
 @method_decorator(csrf_exempt, name='dispatch')
@@ -15,25 +15,41 @@ class BruteForce(View):
     def post(cls, request):
         target_spectrum = loads(request.body).get("target_spectrum")
         peptides = ['']
-        results = []
+        results = {}
         target_peptide_mass = target_spectrum[-1]
+        tree = {
+            "Root": {
+                "mass": 0,
+                "children": [],
+                "end": False
+            }
+        }
 
         while len(peptides) > 0:
-            extended_peptides = extend(peptides)
+            extended_peptides = extend_for_tree(peptides, tree)
 
             candidates = []
 
             for peptide in extended_peptides:
                 peptide_mass = calculate_peptide_mass(peptide)
+                tree[peptide]["mass"] = peptide_mass
                 if peptide_mass == target_peptide_mass:
-                    if cyclic_spectrum(peptide) == target_spectrum:
-                        results.append(peptide)
+                    tree[peptide]["end"] = True
+                    calculated_spectrum, spectrum_with_masses = cyclic_spectrum(peptide)
+                    if calculated_spectrum == target_spectrum:
+                        results[peptide] = spectrum_with_masses
                 elif peptide_mass < target_peptide_mass:
                     candidates.append(peptide)
+                else:
+                    tree[peptide]["end"] = True
 
             peptides = candidates
 
-        return JsonResponse({"results": results}, status=200)
+        response = {
+            "candidates": results,
+            "tree": tree
+        }
+        return JsonResponse(response, status=200)
 
 
 @method_decorator(csrf_exempt, name='dispatch')
@@ -43,27 +59,52 @@ class BranchAndBound(View):
     def post(cls, request):
         target_spectrum = loads(request.body).get("target_spectrum")
         peptides = ['']
-        results = []
+        results = {}
+        tree = {
+            "Root": {
+                "mass": 0,
+                "children": [],
+                "end": False,
+                "candidate": False
+            }
+        }
 
         target_peptide_mass = target_spectrum[-1]
+        solution = []
 
         while len(peptides) > 0:
-            extended_peptides = extend(peptides)
+            extended_peptides = extend_for_tree(peptides, tree)
 
             consistent_peptides = []
 
             for peptide in extended_peptides:
                 peptide_mass = calculate_peptide_mass(peptide)
+                tree[peptide]["mass"] = peptide_mass
                 if peptide_mass == target_peptide_mass:
-                    if cyclic_spectrum(peptide) == target_spectrum:
-                        results.append(peptide)
+                    tree[peptide]["end"] = True
+                    calculated_spectrum, spectrum_with_masses = cyclic_spectrum(peptide)
+                    if calculated_spectrum == target_spectrum:
+                        results[peptide] = spectrum_with_masses
+                        tree[peptide]["candidate"] = True
+                        solution.append(peptide)
+                    else:
+                        tree[peptide]["candidate"] = False
                 elif peptide_mass < target_peptide_mass:
                     if is_consistent_with_spectrum(peptide, target_spectrum):
                         consistent_peptides.append(peptide)
+                else:
+                    tree[peptide]["end"] = True
+                    tree[peptide]["candidate"] = False
+                    tree[peptide]["reason"] = "Peptid ima preveliku masu i ne može biti rešenje."
 
             peptides = consistent_peptides
 
-        return JsonResponse({"results": results}, status=200)
+        response = {
+            "candidates": results,
+            "tree": tree,
+            "solution": solution
+        }
+        return JsonResponse(response, status=200)
 
 
 @method_decorator(csrf_exempt, name='dispatch')
@@ -73,12 +114,7 @@ class Leaderboard(View):
     def post(cls, request):
         target_spectrum = loads(request.body).get("target_spectrum")
 
-        leader_peptide, tree = leaderboard_sequencing(target_spectrum)
-        response = {
-            "leader_peptide": leader_peptide,
-            "tree": tree
-        }
-
+        response = leaderboard_sequencing(target_spectrum)
         return JsonResponse(response, status=200)
 
 
@@ -113,14 +149,16 @@ class SpectralConvolution(View):
             top_masses = [mass for mass, num in sorted_masses if number_of_showing <= num]
 
         amino_acid_candidates = prepare_amino_acids_that_are_candidates(top_masses)
-        leader_peptide, tree = leaderboard_sequencing(target_spectrum, amino_acid_candidates)
+        leaderboard_response = leaderboard_sequencing(target_spectrum, amino_acid_candidates)
 
         response = {
-            "most_common_elements": sorted_masses,
+            "amino_acids_in_peptides": sorted_masses,
             "top": top_masses,
             "amino_acid_candidates": amino_acid_candidates,
-            "leader_peptide": leader_peptide,
-            "tree": tree
+            "leaderboard": leaderboard_response["leaderboard"],
+            "solution": leaderboard_response["solution"],
+            "N": leaderboard_response["N"],
+            "M": cls.NUMBER_OF_LARGEST_ELEMENTS
         }
 
         return JsonResponse(response, status=200)
